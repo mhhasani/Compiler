56893caf4763f139cebdc4596fc0591e690441c0
mhhasani <hasanimohammadhosein@gmail.com>
2023-03-10 17:55:17 +0330

    test_acceptance

--- DFA2Code/bot.py
+++ DFA2Code/bot.py
@@ -9,6 +9,7 @@ from telegram.ext import (Updater,
                           CallbackQueryHandler,
                           ConversationHandler,)
 from main import DFA
+import os
 
 def start(update: Update, context):
     update.message.reply_text(chat_id=update.message.chat_id, text="please send your dfa in the following format:\nstates\ninitial\naccepting\nalphabet\ntransitions\n\nfor example:\nq0 q1 q2 q3\nq0\nq3\n0 1\nq0:0>q0\nq0:1>q1\nq1:0>q2\nq1:1>q0\nq2:0>q3\nq2:1>q1\nq3:0>q2\nq3:1>q0")
@@ -34,10 +35,32 @@ def new_dfa(update: Update, context):
         # send dfa cpp code
         cpp_code_file = open("dfa.cpp", "rb")
         update.message.reply_document(cpp_code_file)
+        # create last dfa python code of user
+        with open(f"{update.message.from_user.id}.py", "w") as f:
+            f.write(dfa.generate_dfa_code())
     except:
         update.message.reply_text("wrong format!\nplease send your dfa in the following format:\nstates\ninitial\naccepting\nalphabet\ntransitions\n\nfor example:\nq0 q1 q2 q3\nq0\nq3\n0 1\nq0:0>q0\nq0:1>q1\nq1:0>q2\nq1:1>q0\nq2:0>q3\nq2:1>q1\nq3:0>q2\nq3:1>q0")
     
     
+def test_acceptance(update: Update, context):
+    user_last_dfa_file = f"{update.message.from_user.id}.py"
+    # check if user has last dfa
+    if os.path.exists(user_last_dfa_file):
+        # import user last dfa
+        import_code = f"from {user_last_dfa_file} import DFA as UserDFA"
+        # execute import code
+        exec(import_code)
+        # create user dfa object
+        user_dfa = UserDFA()
+        # check if user input is accepted by dfa
+        if user_dfa.run(update.message.text):
+            update.message.reply_text("accepted")
+        else:
+            update.message.reply_text("rejected")
+    else:
+        update.message.reply_text("please send your dfa first")
+        
+        
 def main():
     updater = Updater("6167343455:AAFUNe4or98G1x3adbTG5v_uU7MbqsYjHl8")
 
