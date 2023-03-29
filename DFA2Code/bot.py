4aa85f8a58af92a494e61fd2170a9f79c95b4266
mhhasani <hasanimohammadhosein@gmail.com>
2023-03-29 11:25:13 +0330

    deploy heroku

--- DFA2Code/bot.py
+++ DFA2Code/bot.py
@@ -11,6 +11,10 @@ from telegram.ext import (Updater,
 from main import DFA
 import os
 
+TOKEN = "6167343455:AAFUNe4or98G1x3adbTG5v_uU7MbqsYjHl8"
+WEBHOOK_URL = f"DFAtoCode.pythonanywhere.com/{TOKEN}"
+PORT = os.environ.get('PORT', 5000)
+
 class UserDFA:
     def __init__(self, alphabet, states, start_state, accepting_states, transitions):
         self.alphabet = alphabet
@@ -79,8 +83,7 @@ def test_acceptance(update: Update, context):
         
         
 def main():
-    updater = Updater("6167343455:AAFUNe4or98G1x3adbTG5v_uU7MbqsYjHl8")
-
+    updater = Updater(token=TOKEN)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
@@ -93,11 +96,12 @@ def main():
     ))
     dispatcher.add_handler(MessageHandler(Filters.text, new_dfa))
 
+    # Set the webhook for the bot
+    updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url=WEBHOOK_URL)
+    
 
-    updater.start_polling()
-    updater.idle()
-
-main()
+if __name__ == '__main__':
+    main()
 
 
 
