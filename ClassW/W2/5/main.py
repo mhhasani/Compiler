a54daa094db8bbff58a6dd56242f4e817991d6df
mhhasani <hasanimohammadhosein@gmail.com>
2023-04-14 12:25:39 +0330

    w2

---
+++ ClassW/W2/5/main.py
@@ -0,0 +1,26 @@
+import sys
+import os
+# Add the path to the generated parser to the system path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+
+from antlr4 import *
+from CPP14Lexer import CPP14Lexer
+
+def main():
+    lexer = CPP14Lexer(FileStream(r"input.cpp"))
+    output_stream = open(r"output.cpp", "w")
+    token = lexer.nextToken()
+    while token.type != token.EOF:
+        text = token.text
+        if token.type == lexer.BlockComment:
+            text = "// 99521199 " + text[2:-2]
+            text = text.replace("\n", "\n//")
+        elif token.type == lexer.LineComment:
+            text = "/* 99521199 " + text[2:]
+        output_stream.write(text)
+        token = lexer.nextToken()
+
+
+if __name__ == '__main__':
+    main()
